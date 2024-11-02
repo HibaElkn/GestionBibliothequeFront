d4432e7f7852ca4da2197adbbcfdf569cfcd9747
Rben-c <155426152+Rben-c@users.noreply.github.com>
2024-11-02 09:14:03 +0100

    Update userService.js
    suppression des donnees statiques

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,18 +2,7 @@
 const getUsers = async () => {
     // Fetch users from API or local data
     return [
-        { id: 1, codeMassar: 'ABC123', firstName: 'Thomas', lastName: 'Hardy', email: 'thomashardy@mail.com' },
-        { id: 2, codeMassar: 'DEF456', firstName: 'Dominique', lastName: 'Perrier', email: 'dominiqueperrier@mail.com' },
-        { id: 3, codeMassar: 'gg', firstName: 'Thomas', lastName: 'Hardy', email: 'thomashardy@mail.com' },
-        { id: 4, codeMassar: 'h', firstName: 'Dominique', lastName: 'Perrier', email: 'dominiqueperrier@mail.com' },
-        { id: 5, codeMassar: 'hhh', firstName: 'Thomas', lastName: 'Hardy', email: 'thomashardy@mail.com' },
-        { id: 6, codeMassar: 'lll', firstName: 'Dominique', lastName: 'Perrier', email: 'dominiqueperrier@mail.com' },
-        { id: 7, codeMassar: 'hhhhhh', firstName: 'Thomas', lastName: 'Hardy', email: 'thomashardy@mail.com' },
-        { id: 8, codeMassar: 'rgtesdc', firstName: 'Dominique', lastName: 'Perrier', email: 'dominiqueperrier@mail.com' },
-        { id: 9, codeMassar: 'mmmm', firstName: 'Thomas', lastName: 'Hardy', email: 'thomashardy@mail.com' },
-        { id: 10, codeMassar: 'vgtfr', firstName: 'Dominique', lastName: 'Perrier', email: 'dominiqueperrier@mail.com' },
-        { id: 11, codeMassar: 'mpoo', firstName: 'Thomas', lastName: 'Hardy', email: 'thomashardy@mail.com' },
-        { id: 12, codeMassar: 'fgvtgt', firstName: 'Dominiquesss', lastName: 'Perrier', email: 'dominiqueperrier@mail.com' },
+      
     ];
 };
 
