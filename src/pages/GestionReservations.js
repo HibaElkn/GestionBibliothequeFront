9f87fd0c33ab230ed594fe28871722311f307260
HibaElkn <137877795+HibaElkn@users.noreply.github.com>
2024-12-01 14:37:47 +0100

    Update GestionReservations.js

--- src/pages/GestionReservations.js
+++ src/pages/GestionReservations.js
@@ -1,62 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css'; // Assurez-vous d'importer FontAwesome
 import '../styles/Emprunts.css';
-import { getAllReservations } from '../services/reservationService'; // Assurez-vous d'importer la fonction
-import documentService from '../services/documentService'; // Assurez-vous d'importer la fonction pour récupérer le titre du document
-import userService from '../services/userService'; // Assurez-vous d'importer la fonction pour récupérer les informations de l'utilisateur
 
-const GestionReservations = ({ onDeleteReservation, onAddReservation }) => {
+const GestionReservations = ({ onUpdateHistorique }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage] = useState(7);
-    const [showAddReservationPopup, setShowAddReservationPopup] = useState(false);
-    const [newReservation, setNewReservation] = useState({
-        code: '',
-        nomPrenom: '',
-        titreLivre: '',
-        dateReservation: '',
-        statut: 'en attente', // Valeur par défaut
-    });
+    const [reservationsData, setReservationsData] = useState([
+        {
+            id: 1,
+            cne: '123456',
+            numSom: '7890',
+            nomPrenom: 'John Doe',
+            titreLivre: 'Le Grand Livre',
+            dateReservation: '2024-11-20',
+            statut: 'en attente',
+        },
+        {
+            id: 2,
+            cne: '234567',
+            numSom: '8901',
+            nomPrenom: 'Jane Smith',
+            titreLivre: 'Les Mystères de l\'univers',
+            dateReservation: '2024-11-21',
+            statut: 'en attente',
+        },
+        {
+            id: 3,
+            cne: '345678',
+            numSom: '9012',
+            nomPrenom: 'Marc Dupont',
+            titreLivre: 'Le Voyage Extraordinaire',
+            dateReservation: '2024-11-22',
+            statut: 'en attente',
+        }
+    ]);
 
-    const [reservationsData, setReservationsData] = useState([]); // Initialisez avec un tableau vide
-
-    // Utilisez useEffect pour charger les réservations à partir de l'API
-    useEffect(() => {
-        const fetchReservations = async () => {
-            try {
-                const data = await getAllReservations(); // Récupérer les données via l'API
-                // Ajout des titres des documents et des informations utilisateur
-                const reservationsWithDetails = await Promise.all(
-                    data.map(async (reservation) => {
-                        try {
-                            const document = await documentService.getDocumentById(reservation.documentId); // Récupère le document
-                            const user = await userService.getUserById(reservation.utilisateurId); // Récupère l'utilisateur
-
-                            return {
-                                ...reservation,
-                                titreDocument: document.titre || "Titre non disponible", // Ajoute le titre du document
-                                code: user?.code || "code non disponible", // Ajoute le code de l'utilisateur
-                                nomPrenom: user ? `${user.nom} ${user.prenom}` : "Nom non disponible", // Nom et prénom de l'utilisateur
-                            };
-                        } catch (err) {
-                            console.error(`Erreur lors de la récupération des détails pour la réservation ID: ${reservation.id}`, err);
-                            return {
-                                ...reservation,
-                                titreDocument: "Titre non disponible", // Valeur par défaut en cas d'erreur
-                                code: "code non disponible", // Valeur par défaut pour code
-                                nomPrenom: "Nom non disponible", // Valeur par défaut pour le nom
-                            };
-                        }
-                    })
-                );
-                setReservationsData(reservationsWithDetails); // Mettre à jour les données des réservations avec les titres et les informations utilisateur
-            } catch (error) {
-                console.error('Erreur lors de la récupération des réservations:', error);
-            }
-        };
-
-        fetchReservations();
-    }, []); // Ce useEffect se déclenche uniquement lors du premier montage du composant
+    const [historiqueReservations, setHistoriqueReservations] = useState([]);
 
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -65,35 +45,13 @@ const GestionReservations = ({ onDeleteReservation, onAddReservation }) => {
 
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    const handleDeleteReservation = (id) => {
-        if (window.confirm("Êtes-vous sûr de vouloir supprimer cette réservation ?")) {
-            onDeleteReservation(id);
-        }
-    };
-
-    const handleAddReservationChange = (e) => {
-        setNewReservation({ ...newReservation, [e.target.name]: e.target.value });
-    };
-
-    const handleAddReservationSubmit = () => {
-        onAddReservation(newReservation); // Appel à la fonction d'ajout
-        setShowAddReservationPopup(false); // Ferme la popup
-        setNewReservation({
-            code: '',
-            nomPrenom: '',
-            titreLivre: '',
-            dateReservation: '',
-            statut: 'en attente', // Réinitialise le statut à "en attente"
-        }); // Réinitialise le formulaire
-    };
-
     const getStatutClass = (statut) => {
         switch (statut) {
-            case 'ACCEPTED':
+            case 'confirmé':
                 return 'bg-success text-white';
-            case 'ENCOURS':
+            case 'en attente':
                 return 'bg-warning text-dark';
-            case 'REJECTED':
+            case 'refusé':
                 return 'bg-danger text-white';
             default:
                 return '';
@@ -101,36 +59,28 @@ const GestionReservations = ({ onDeleteReservation, onAddReservation }) => {
     };
 
     const handleStatutChange = (id, newStatut) => {
-        const updatedReservations = reservationsData.map(reservation => 
-            reservation.id === id ? { ...reservation, statut: newStatut } : reservation
-        );
-        setReservationsData(updatedReservations);
-    };
+        const reservationToUpdate = reservationsData.find(reservation => reservation.id === id);
 
-    const [editingStatut, setEditingStatut] = useState(null); // State pour gérer quel statut est en cours d'édition
+        if (reservationToUpdate) {
+            const updatedReservation = { ...reservationToUpdate, statut: newStatut };
+            setHistoriqueReservations(prev => [...prev, updatedReservation]);
+
+            const updatedReservations = reservationsData.filter(reservation => reservation.id !== id);
+            setReservationsData(updatedReservations);
+
+            if (onUpdateHistorique) {
+                onUpdateHistorique([...historiqueReservations, updatedReservation]);
+            }
+        }
+    };
 
     return (
         <div className="container">
-            <div className="d-flex justify-content-end mb-3">
-                <button 
-                    onClick={() => setShowAddReservationPopup(true)} 
-                    className="btn btn-primary custom-btn"
-                    style={{
-                        backgroundColor: '#D99A22', 
-                        color: '#004079ff',           
-                        border: '1px solid #D99A22', 
-                        padding: '10px 20px',       
-                        borderRadius: '5px',        
-                    }}>
-                    <i className="fas fa-plus"></i> Ajouter une réservation
-                </button>
-            </div>
-
             <div className="table-wrapper">
                 <table className="table table-striped table-hover">
                     <thead>
                         <tr>
-                            <th>Code</th>
+                            <th>CNE / Num de Som</th>
                             <th>Nom et Prénom</th>
                             <th>Titre du Livre</th>
                             <th>Date de réservation</th>
@@ -141,43 +91,27 @@ const GestionReservations = ({ onDeleteReservation, onAddReservation }) => {
                     <tbody>
                         {currentItems.map(reservation => (
                             <tr key={reservation.id}>
-                                <td>{reservation.code}</td> {/* Affiche code */}
-                                <td>{reservation.nomPrenom}</td> {/* Affiche nom et prénom */}
-                                <td>{reservation.titreDocument}</td> {/* Affichage du titre du document */}
+                                <td>{reservation.cne} / {reservation.numSom}</td>
+                                <td>{reservation.nomPrenom}</td>
+                                <td>{reservation.titreLivre}</td>
                                 <td>{reservation.dateReservation}</td>
                                 <td>
-                                    <span className={`badge ${getStatutClass(reservation.reservationStatus)} rounded-3`}>
-                                        {reservation.reservationStatus}
+                                    <span className={`badge ${getStatutClass(reservation.statut)} rounded-3`}>
+                                        {reservation.statut}
                                     </span>
-                                    
-                                    <button 
-                                        className="btn btn-sm btn-info ms-2" 
-                                        onClick={() => setEditingStatut(reservation.id)}
-                                    >
-                                        <i className="fa fa-pencil-alt"></i>
-                                    </button>
-
-                                    {editingStatut === reservation.id && (
-                                        <select 
-                                            value={reservation.statut} 
-                                            onChange={(e) => {
-                                                handleStatutChange(reservation.id, e.target.value);
-                                                setEditingStatut(null); 
-                                            }}
-                                            className="form-select form-select-sm ms-2"
-                                        >
-                                            <option value="confirmé">Confirmé</option>
-                                            <option value="en attente">En attente</option>
-                                            <option value="refusé">Refusé</option>
-                                        </select>
-                                    )}
                                 </td>
                                 <td>
-                                    <button 
-                                        onClick={() => handleDeleteReservation(reservation.id)} 
-                                        className="btn btn-sm" 
-                                        style={{ color: 'red' }} >
-                                        <i className="fas fa-trash-alt"></i>
+                                    <button
+                                        className="btn btn-sm text-success"
+                                        onClick={() => handleStatutChange(reservation.id, 'confirmé')}
+                                    >
+                                        ✔
+                                    </button>
+                                    <button
+                                        className="btn btn-sm text-danger"
+                                        onClick={() => handleStatutChange(reservation.id, 'refusé')}
+                                    >
+                                        ✘
                                     </button>
                                 </td>
                             </tr>
