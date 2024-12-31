01d803a5c1942ea12b7f55b93102f523d748a9d8
Rben-c <155426152+Rben-c@users.noreply.github.com>
2024-12-31 20:35:51 +0100

    Update GestionPersonnel.js

--- src/pages/GestionPersonnel.js
+++ src/pages/GestionPersonnel.js
@@ -43,9 +43,9 @@ const GestionPersonnel = () => {
                 prenom: user.prenom,
                 email: user.email,
                 code: user.code, // 'Numéro de SOM'
-                //role: "PERSONNEL"
+                role: user.role
             };
-            const updatedData = await userService.updateUser('personnel', user.id, updatedUser); // Mise à jour basée sur l'email
+            const updatedData = await userService.updateUser( user.id, updatedUser); // Mise à jour basée sur l'email
             setUsers(prevUsers => 
                 prevUsers.map(u => u.email === user.email ? updatedData : u) // Mettre à jour l'utilisateur dans la liste
             );
